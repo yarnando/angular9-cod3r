beb90d47b58beb669c4c769eb742dc83ef1f0222
Nando <fernandosilva.smsrio@gmail.com>
2020-06-17 19:08:41 -0300

    Aula 9 - Navbar/importação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/template/header/header.component';
-import { FooterComponent } from './components/template/footer/footer.component'
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
