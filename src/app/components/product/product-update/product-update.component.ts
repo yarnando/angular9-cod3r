9b8544b26a4747d43e3eaf5733692d163158ee23
Nando <fernandosilva.smsrio@gmail.com>
2020-06-21 04:46:02 -0300

    Aula 32 - Update funcionando!

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
   }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado!')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }  
 
 }
